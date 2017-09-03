e6b0c1fc96fa101e04caa8092bdab7705f75e5a7
pastiwibawa <pastiwibawa@gmail.com>
2017-09-03 08:55:53 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MdButtonModule, MdCardModule, MdInputModule, MdSidenavModule, MdToolbarModule, MdListModule, MdIconModule, MdDialogModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent, AddContactDialog } from './contacts/contacts.component';
+import { PopularComponent } from './popular/popular.component';
+import { ContactsService } from "./contacts.service";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PopularComponent
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent
+  },
+  {
+    path: 'popular',
+    component: PopularComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    PopularComponent,
+    AddContactDialog
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MdButtonModule,
+    MdCardModule,
+    MdInputModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdListModule,
+    MdIconModule,
+    MdDialogModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ContactsService],
+  bootstrap: [AppComponent],
+  entryComponents: [AddContactDialog]
 })
 export class AppModule { }
